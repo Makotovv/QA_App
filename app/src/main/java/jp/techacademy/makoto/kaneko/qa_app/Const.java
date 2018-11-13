4deaa772757c50842ec3f8ba9a2a86fdb9836a91
Makotovv <ma-kasbs757@chic.ocn.ne.jp>
2018-11-13 22:22:49 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/makoto/kaneko/qa_app/Const.java
@@ -0,0 +1,9 @@
+package jp.techacademy.makoto.kaneko.qa_app;
+
+public class Const {
+    public static final String UsersPATH = "users";            // ユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents";       // 質問を保存するバス
+    public static final String AnswersPATH = "answers";       // 解答を保存するパス
+
+    public static final String NameKEY = "name";             // 表示名を保存する時のキー
+}
